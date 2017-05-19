c84c527bc9a9c20ad28295348375871445afdc17
JulieLaco <lacognata@intechinfo.fr>
2017-05-19 11:12:18 +0200

    Add basic informations into IQuest and QuestController

--- Assets/Scripts/Quest/Interfaces/IQuest.cs
+++ Assets/Scripts/Quest/Interfaces/IQuest.cs
@@ -7,5 +7,36 @@ namespace Assets.Scripts.Quest.Interfaces
 {
     interface IQuest
     {
+        /// <summary>
+        /// Gets the quest identifier.
+        /// </summary>
+        /// <value>
+        /// The quest identifier.
+        /// </value>
+        int QuestID { get; }
+
+        /// <summary>
+        /// Gets the name of the quest.
+        /// </summary>
+        /// <value>
+        /// The name of the quest.
+        /// </value>
+        string QuestName { get; }
+
+        /// <summary>
+        /// Gets the quest description.
+        /// </summary>
+        /// <value>
+        /// The quest description.
+        /// </value>
+        string QuestDescription { get; }
+
+        /// <summary>
+        /// Gets the quest dialogue.
+        /// </summary>
+        /// <value>
+        /// The quest dialogue.
+        /// </value>
+        string QuestDialogue { get; }
     }
 }
--- Assets/Scripts/Quest/QuestController.cs
+++ Assets/Scripts/Quest/QuestController.cs
@@ -1,18 +1,64 @@
-﻿using System;
+﻿using Assets.Scripts.Quest.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace Assets.Scripts.Quest
 {
-    class QuestController
+    class QuestController : IQuest
     {
-        int _questId;
+        int _questID;
         string _questName;
         string _questDescription;
-        int _questSteps; //a préciser entre une classe elle même, un string ou un int comme ici
+        string _questDialogue;
+        //int _questSteps; a préciser entre une classe elle même, un string ou un int comme ici
 
         ObjectLibrary _objectLibrary; //appel de la classe objectLibrary qui contient les objets de la quête sous forme de liste 
+
+        /// <summary>
+        /// Gets the quest identifier.
+        /// </summary>
+        /// <value>
+        /// The quest identifier.
+        /// </value>
+        public int QuestID
+        {
+            get { return _questID; }
+        }
+
+        /// <summary>
+        /// Gets the name of the quest.
+        /// </summary>
+        /// <value>
+        /// The name of the quest.
+        /// </value>
+        public string QuestName
+        {
+            get { return _questName; }
+        }
+
+        /// <summary>
+        /// Gets the quest description.
+        /// </summary>
+        /// <value>
+        /// The quest description.
+        /// </value>
+        public string QuestDescription
+        {
+            get { return _questDescription; }
+        }
+
+        /// <summary>
+        /// Gets the quest dialogue.
+        /// </summary>
+        /// <value>
+        /// The quest dialogue.
+        /// </value>
+        public string QuestDialogue
+        {
+            get { return _questDialogue; }
+        }
 
         //QuestLog (à définir + à implémenter)
         //EventLog (à définir + à implémenter)
