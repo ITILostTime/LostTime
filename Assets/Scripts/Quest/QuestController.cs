1156f28cdcd0f7be52b40a412d449f5e1ce6d4be
ychergui <chergui@intechinfo.fr>
2017-05-19 11:31:13 +0200

    Add some summary

--- Assets/Scripts/Quest/QuestController.cs
+++ Assets/Scripts/Quest/QuestController.cs
@@ -17,21 +17,45 @@ namespace Assets.Scripts.Quest
         
         ObjectLibrary _objectLibrary; //Call class ObjectLibrary (contains the quest in the form of list).
 
+        /// <summary>
+        /// Gets the quest identifier.
+        /// </summary>
+        /// <value>
+        /// The quest identifier.
+        /// </value>
         public int QuestID
         {
             get { return questInformations.QuestID; }
         }
 
+        /// <summary>
+        /// Gets the name of the quest.
+        /// </summary>
+        /// <value>
+        /// The name of the quest.
+        /// </value>
         public string QuestName
         {
             get { return questInformations.QuestName; }
         }
 
+        /// <summary>
+        /// Gets the quest description.
+        /// </summary>
+        /// <value>
+        /// The quest description.
+        /// </value>
         public string QuestDescription
         {
             get { return questInformations.QuestDescription; }
         }
 
+        /// <summary>
+        /// Gets the quest dialogue.
+        /// </summary>
+        /// <value>
+        /// The quest dialogue.
+        /// </value>
         public string QuestDialogue
         {
             get { return questInformations.QuestDialogue; }
