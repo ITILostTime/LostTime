29a8b61e7e727904baa47e2e04ed08001705cb94
JulieLaco <lacognata@intechinfo.fr>
2017-05-31 11:35:06 +0200

    Add begin of parseJson system

---
+++ LostTimeGame.Tests/QuestSystemTests/JsonParser/QuestJsonParserTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+
+namespace LostTimeGame.Tests.QuestSystemTests.JsonParser
+{
+    [TestFixture]
+    class QuestJsonParserTests
+    {
+        [Test]
+        public void Parse_json_file()
+        {
+
+        }
+    }
+}
