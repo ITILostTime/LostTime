3b2a26bf5aa4ddeca0ceb2dbb688ad390eb72254
JulieLaco <lacognata@intechinfo.fr>
2017-06-06 11:21:57 +0200

    Add package Newtonsoft

--- LostTimeGame.Tests/QuestSystemTests/QuestJsonParserTests.cs
+++ LostTimeGame.Tests/QuestSystemTests/QuestJsonParserTests.cs
@@ -10,6 +10,8 @@ using System.Runtime.Serialization.Json;
 using System.IO;
 using SimpleJSON;
 
+using Newtonsoft.Json;
+
 namespace LostTimeGame.Tests.QuestSystemTests.JsonParser
 {
     [TestFixture]
@@ -18,12 +20,20 @@ namespace LostTimeGame.Tests.QuestSystemTests.JsonParser
         [Test]
         public void Read_JSON_file()
         {
+            Quest quest1 = JsonConvert.DeserializeObject<Quest>(File.ReadAllText(@"..\..\QuestSystemTests\JsonParser\QuestTest.json"));
+
+            using (StreamReader file = File.OpenText(@"..\..\QuestSystemTests\JsonParser\QuestTest.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                Quest quest2 = (Quest)serializer.Deserialize(file, typeof(Quest));
+            }
+
             //Theoretical test
-            string[] n = System.IO.File.ReadAllLines(@"..\..\QuestSystemTests\JsonParser\QuestTest.json");
+            /*string[] n = System.IO.File.ReadAllLines(@"..\..\QuestSystemTests\JsonParser\QuestTest.json");
             Console.WriteLine(n);
 
             string[] o = System.IO.File.ReadAllLines(@"..\..\QuestSystemTests\JsonParser\QuestTest1.json");
-            Console.WriteLine(o);
+            Console.WriteLine(o);*/
         }
     }
 }
