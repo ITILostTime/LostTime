819005b59519d050f1d627e1fd3d997df535041c
GElisabeth <guillaume.elisabeth1836@gmail.com>
2017-06-28 12:34:44 +0200

    pathfinding script opti

--- Assets/Scripts/PNJQuest/PNJPathfinding.cs
+++ Assets/Scripts/PNJQuest/PNJPathfinding.cs
@@ -1,21 +1,49 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class PNJPathfinding : MonoBehaviour {
-    
-    public GameObject _target;
+public class PNJPathfinding : MonoBehaviour
+{
+
+    private GameObject _target;
 
     private void Start()
     {
         this.gameObject.AddComponent<NavMeshAgent>();
         this.gameObject.GetComponent<NavMeshAgent>().radius = 1;
         this.gameObject.GetComponent<NavMeshAgent>().height = 3.5f;
+        this.gameObject.GetComponent<NavMeshAgent>().speed = 1.5f;
+        FindTarget();
+    }
+
+    private void FindTarget()
+    {
+        System.Random n = new System.Random();
+        GameObject tmpWayPoints = GameObject.Find("Waypoint (" + n.Next(0, 8) + ")");
+
+
+        if (tmpWayPoints != _target)
+        {
+            _target = tmpWayPoints;
+        }
+        else
+        {
+            FindTarget();
+        }
     }
 
     private void Update()
     {
         this.GetComponent<NavMeshAgent>().destination = _target.transform.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == _target)
+        {
+            FindTarget();
+        }
     }
 }
